2a5407972049d02db004b0dd9a0dbc0484190756
pashudzu <pavelcrosot@gmail.com>
2025-11-04 12:24:21 +0000

    chore: remove trash files and add .gitignore

--- target/debug/deps/volitionos-5eba63481259b243.d
+++
@@ -1,5 +0,0 @@
-/home/pashudzu/Projects/volitionos/target/debug/deps/volitionos-5eba63481259b243.d: src/main.rs
-
-/home/pashudzu/Projects/volitionos/target/debug/deps/libvolitionos-5eba63481259b243.rmeta: src/main.rs
-
-src/main.rs:
--- target/debug/volitionos.d
+++
@@ -1,1 +0,0 @@
-/home/pashudzu/Projects/volitionos/target/debug/volitionos: /home/pashudzu/Projects/volitionos/src/main.rs
