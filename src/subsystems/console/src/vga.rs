38804a765db506f932da628db664dcea5b58cb1d
pashudzu <pavelcrosot@gmail.com>
2025-11-09 13:37:54 +0300

    Add: color enum

--- src/subsystems/console/src/vga.rs
+++ src/subsystems/console/src/vga.rs
@@ -1,5 +1,26 @@
 use core::fmt::{Error, Write};
 
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+#[repr(u8)]
+pub enum Color {
+    Black = 0,
+    Blue = 1, 
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11, 
+    LightRed = 12, 
+    Pink = 13,
+    Yellow = 14,
+    White = 15,
+}
+
 struct Console;
 
 impl Write for Console {
